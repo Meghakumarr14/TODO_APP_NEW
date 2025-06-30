78288f34c7024ac0201e88a00eee88bfffa3645c
megha <kumarmegha54@gmail.com>
2025-07-01 00:15:20 +0530

    Updated backend url

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,17 +1,19 @@
 const Login = () => {
+  const backendURL = process.env.REACT_APP_BACKEND_URL;
+
   return (
     <div className="login-container">
       <h2>Login With</h2>
 
-      <a href="http://localhost:5000/api/auth/google">
+      <a href={`${backendURL}/api/auth/google`}>
         <button className="google">Login with Google</button>
       </a>
 
-      <a href="http://localhost:5000/api/auth/github">
+      <a href={`${backendURL}/api/auth/github`}>
         <button className="github">Login with GitHub</button>
       </a>
 
-      <a href="http://localhost:5000/api/auth/facebook">
+      <a href={`${backendURL}/api/auth/facebook`}>
         <button className="facebook">Login with Facebook</button>
       </a>
     </div>
